779629be27270445e71c7aa7038e2420086ed97f
Yang Guo <gy@fir.im>
2018-10-30 17:31:49 +0800

    create js for send dingtalk

---
+++ dingtalk.js
@@ -0,0 +1,89 @@
+const https = require("https");
+const url = require('url');
+const envsub = require('envsub');
+
+const Token = process.env.FLOWCI_DING_TOKEN;
+const DingTalkUrl = "https://oapi.dingtalk.com/robot/send?access_token=" + Token;
+
+console.log = function (msg) {
+  process.stdout.write(msg + '\n');
+};
+
+console.error = function (msg) {
+  process.stderr.write(msg + '\n');
+}
+
+console.log('#################### DingTalk ####################')
+
+createMessage().then((data) => {
+  // send(data);
+});
+
+async function createMessage() {
+  let templateFile = `${__dirname}/template.md`;
+
+  return {
+    "msgtype": "markdown",
+    "markdown": {
+      "title": "flow.ci",
+      "text": await readTemplate()
+    }
+  };
+}
+
+function readTemplate() {
+  let options = {
+    diff: false,
+    system: true,
+    syntax: 'default'
+  };
+
+  let templateFile = `${__dirname}/template.md`;
+
+  return new Promise((resolve, reject) => {
+    envsub({templateFile, options}).then((envobj) => {
+      resolve(envobj.outputContents);
+    }).catch((err) => {
+      console.error(err);
+      process.exit(1)
+    });
+  });
+}
+
+function send(data) {
+  const dataInStr = JSON.stringify(data);
+  const options = url.parse(DingTalkUrl);
+  options.method = 'POST'
+  options.headers = {
+    'Content-Type': 'application/json',
+    'Content-Length': dataInStr.length
+  }
+
+  const req = https.request(options, (res) => {
+    res.on('data', (d) => {
+      const data = JSON.parse(d);
+      onResponse(data);
+    });
+  });
+
+  req.on('error', (e) => {
+    console.error(e);
+    process.exit(1);
+  });
+
+  req.write(dataInStr);
+  req.end();
+}
+
+/**
+ * Handle DingTalk response Ex: {"errmsg":"ok","errcode":0}
+ */
+function onResponse(response) {
+  if (response.errcode == 0) {
+    console.log(response.errmsg);
+    process.exit();
+  }
+
+  console.error(response.errmsg)
+  process.exit(1)
+}
